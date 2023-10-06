3e99307a971409448f7ad6231d52d649dc803f8c
kazu51-gh <kazu51git@gmail.com>
2023-10-06 14:27:47 +0900

    create and design textarea

--- src/components/workspace.tsx
+++ src/components/workspace.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Workspace() {
+  const [textareaData, setTextareaData] = useState('');
+
   return(
     <div className="flex">
       <div className="flex-1">
-        <textarea className="border border-black m-3 w-11/12" />
+        <textarea
+          className="block border border-black mx-auto p-3 resize-none rounded-lg text-xl w-11/12"
+          rows={25}
+          onChange={(e) => setTextareaData(e.target.value)}
+          placeholder="HTMLを作成していきましょう。"
+        />
       </div>
       <div className="flex-1">工事中...</div>
     </div>
