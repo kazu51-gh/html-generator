6c89c8e6dda407d64a7e933b92549f1fa9ccb35e
kazu51-gh <kazu51git@gmail.com>
2023-10-07 14:36:28 +0900

    carve into accordion component

---
+++ src/components/accordion.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  title: string,
+  description: string
+}
+
+const Accordion = ({title, description}: Props) => {
+  return(
+    <details className="group">
+      <summary className="flex cursor-pointer list-none items-center justify-between p-4 text-lg font-medium text-secondary-900 group-open:bg-gray-50">
+        {title}
+        <div className="text-secondary-500">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="block h-5 w-5 transition-all duration-300 group-open:rotate-180">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          </svg>
+        </div>
+      </summary>
+      <div className="border-t border-t-gray-100 p-4 text-secondary-500">
+        {description}
+        <ul>
+          <li draggable="true">ドラッグするタグブロック</li>
+        </ul>
+      </div>
+    </details>
+  );
+}
+
+export default Accordion;
