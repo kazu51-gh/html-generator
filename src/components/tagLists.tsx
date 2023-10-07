865b6d36d165dda85ec3f056944177a690cd6eff
kazu51-gh <kazu51git@gmail.com>
2023-10-08 02:09:40 +0900

    add tag description

--- src/components/tagLists.tsx
+++ src/components/tagLists.tsx
@@ -21,22 +21,22 @@ const TagLists = () => {
       <div className="h-2/3 mt-4 overflow-y-scroll max-w-xl">
         <div className="divide-y divide-gray-100 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm">
           <Accordion title="<h1> ~ <h6> : 見出し" description={tagDescription.h} />
-          <Accordion title="<p> : 本文" description="xxx" />
-          <Accordion title="<a> : リンク" description="xxx" />
-          <Accordion title="<img> : 画像" description="xxx" />
-          <Accordion title="<ul> : 順序なしの箇条書き" description="xxx" />
-          <Accordion title="<ol> : 順序ありの箇条書き" description="xxx" />
-          <Accordion title="<div> : ブロック要素のコンテナ" description="xxx" />
-          <Accordion title="<span> : インライン要素のコンテナ" description="xxx" />
-          <Accordion title="<br> : 改行" description="xxx" />
-          <Accordion title="<hr> : 水平線" description="xxx" />
-          <Accordion title="<strong> : 強調表示" description="xxx" />
-          <Accordion title="<blockquote> : 引用" description="xxx" />
-          <Accordion title="<code> : コード表示" description="xxx" />
-          <Accordion title="<input> : 入力フォーム" description="xxx" />
-          <Accordion title="<textarea> : テキストエリア" description="xxx" />
-          <Accordion title="<button> : ボタン" description="xxx" />
-          <Accordion title="<table> : テーブル" description="xxx" />
+          <Accordion title="<p> : 本文" description={tagDescription.p} />
+          <Accordion title="<a> : リンク" description={tagDescription.a} />
+          <Accordion title="<img> : 画像" description={tagDescription.img} />
+          <Accordion title="<ul> : 順序なしの箇条書き" description={tagDescription.ul} />
+          <Accordion title="<ol> : 順序ありの箇条書き" description={tagDescription.ol} />
+          <Accordion title="<div> : ブロック要素のコンテナ" description={tagDescription.div} />
+          <Accordion title="<span> : インライン要素のコンテナ" description={tagDescription.span} />
+          <Accordion title="<br> : 改行" description={tagDescription.br} />
+          <Accordion title="<hr> : 水平線" description={tagDescription.hr} />
+          <Accordion title="<strong> : 強調表示" description={tagDescription.strong} />
+          <Accordion title="<blockquote> : 引用" description={tagDescription.blockquote} />
+          <Accordion title="<code> : コード表示" description={tagDescription.code} />
+          <Accordion title="<input> : 入力フォーム" description={tagDescription.input} />
+          <Accordion title="<textarea> : テキストエリア" description={tagDescription.textarea} />
+          <Accordion title="<button> : ボタン" description={tagDescription.button} />
+          <Accordion title="<table> : テーブル" description={tagDescription.table} />
         </div>
       </div>
     </div>
