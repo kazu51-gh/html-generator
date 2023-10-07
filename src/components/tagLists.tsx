bb1d01bdaed7e8939a7c304fda9ff9ca1a17a266
kazu51-gh <kazu51git@gmail.com>
2023-10-07 23:22:49 +0900

    add accordion list

--- src/components/tagLists.tsx
+++ src/components/tagLists.tsx
@@ -6,7 +6,7 @@ const BASE_PATH = basePath ? basePath : '';
 
 const TagLists = () => {
   return(
-    <div>
+    <div className="h-screen">
       <div className="flex">
         <h2 className="mr-2 text-2xl">タグ一覧</h2>
         <div className="flex items-center">
@@ -17,12 +17,25 @@ const TagLists = () => {
           />
         </div>
       </div>
-      <div className="mt-4 max-w-xl">
+      <div className="h-2/3 mt-4 overflow-y-scroll max-w-xl">
         <div className="divide-y divide-gray-100 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm">
-          <Accordion title="title" description="xxx" />
-          <Accordion title="title" description="xxx" />
-          <Accordion title="title" description="xxx" />
-          <Accordion title="title" description="xxx" />
+          <Accordion title="<h1> ~ <h6> : 見出し" description="xxx" />
+          <Accordion title="<p> : 本文" description="xxx" />
+          <Accordion title="<a> : リンク" description="xxx" />
+          <Accordion title="<img> : 画像" description="xxx" />
+          <Accordion title="<ul> : 順序なしの箇条書き" description="xxx" />
+          <Accordion title="<ol> : 順序ありの箇条書き" description="xxx" />
+          <Accordion title="<div> : ブロック要素のコンテナ" description="xxx" />
+          <Accordion title="<span> : インライン要素のコンテナ" description="xxx" />
+          <Accordion title="<br> : 改行" description="xxx" />
+          <Accordion title="<hr> : 水平線" description="xxx" />
+          <Accordion title="<strong> : 強調表示" description="xxx" />
+          <Accordion title="<blockquote> : 引用" description="xxx" />
+          <Accordion title="<code> : コード表示" description="xxx" />
+          <Accordion title="<input> : 入力フォーム" description="xxx" />
+          <Accordion title="<textarea> : テキストエリア" description="xxx" />
+          <Accordion title="<button> : ボタン" description="xxx" />
+          <Accordion title="<table> : テーブル" description="xxx" />
         </div>
       </div>
     </div>
