7696e5a40e484b75f6457c3c2c2a8c8f5f0185ff
kazu51-gh <kazu51git@gmail.com>
2023-10-30 01:29:09 +0900

    make copy code system

--- src/components/tagList/displayCode.tsx
+++ src/components/tagList/displayCode.tsx
@@ -12,9 +12,26 @@ type Props = {
 
 const DisplayCode: FC<Props> = ({ tagName, attributes, content, lists, columns, rows }) => {
   const generateElement = new GenerateElement();
+  const code = generateElement.generateElement(tagName, attributes, content, lists, columns, rows);
+
+  const copyCode = (code: string) => {
+    navigator.clipboard.writeText(code).then(
+      () => { console.log('コピー成功'); },
+      () => { alert('コピー失敗'); }
+    )
+  }
+
   return(
-    <div className="bg-gray-200 inline-block my-3 px-3 py-1.5 whitespace-pre-wrap dark:bg-gray-500 dark:text-gray-300">
-      {generateElement.generateElement(tagName, attributes, content, lists, columns, rows)}
+    <div>
+      <div className="bg-gray-200 inline-block my-3 px-3 py-1.5 whitespace-pre-wrap dark:bg-gray-500 dark:text-gray-300">
+        {code}
+      </div>
+      <button
+        className="bg-white border border-black font-medium inline-block mx-2 my-3 px-2 py-1 rounded text-base text-black hover:bg-slate-100 dark:bg-gray-700 dark:border-gray-500 dark:text-gray-300"
+        onClick={() => copyCode(code)}
+      >
+        コードをコピー
+      </button>
     </div>
   );
 }
