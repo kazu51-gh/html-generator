0019a9c936ee7702ab16b06b2619e3dd385cc696
kazu51-gh <kazu51git@gmail.com>
2023-11-04 03:19:22 +0900

    change pointer for question mark

--- src/components/headings/pageTitle.tsx
+++ src/components/headings/pageTitle.tsx
@@ -8,7 +8,9 @@ const PageTitle: FC<Props> = ({ title }) => {
   return(
     <div className="flex flex-row items-center w-full">
       <h2 className="text-2xl">{title}</h2>
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"
+        className="cursor-pointer"
+      >
         <circle cx="16" cy="16" r="10" fill="white" stroke="black" />
         <text x="8" y="22" font-size="16" font-family="inherit">？</text>
       </svg>
