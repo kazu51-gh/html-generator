b809b29f183bcfe78facc80d12194d8d5b6861d6
kazu51-gh <kazu51git@gmail.com>
2023-11-08 04:41:05 +0900

    add HOME information

---
+++ src/components/headings/functionDescription.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+type Props = {
+  title: string;
+  description: string;
+}
+
+const FunctionDescription: FC<Props> = ({ title, description }) => {
+  return(
+    <div className="mb-6">
+      <h3 className="text-xl mb-1">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default FunctionDescription;
--- src/components/layout/home.tsx
+++ src/components/layout/home.tsx
@@ -1,9 +1,19 @@
-import PageTitle from "../headings/pageTitle";
+import { headingDescriptions } from "@/data/headingData";
+import FunctionDescription from "@/components/headings/functionDescription";
 
 const Home = () => {
   return(
     <div>
-      <PageTitle title="ようこそ" />
+      <h2 className="text-4xl">ようこそ</h2>
+      <p className="text-xl m-6">{headingDescriptions.home}</p>
+      <h2 className="text-2xl mb-3">ページ構成</h2>
+      <div className="ml-6">
+        <FunctionDescription title="(共通) コードエリア" description={headingDescriptions.codeArea} />
+        <FunctionDescription title="ページ情報設定" description={headingDescriptions.pageSetting} />
+        <FunctionDescription title="タグ一覧" description={headingDescriptions.tagLists} />
+        <FunctionDescription title="プレビュー" description={headingDescriptions.viewer} />
+        <FunctionDescription title="ダウンロード" description={headingDescriptions.download} />
+      </div>
     </div>
   );
 }
