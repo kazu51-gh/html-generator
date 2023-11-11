f7ea135ccb1c75feed5cc6808f224e44ad456758
kazu51-gh <kazu51git@gmail.com>
2023-11-11 17:05:59 +0900

    add input string restriction

--- src/components/tagList/idAndClass.tsx
+++ src/components/tagList/idAndClass.tsx
@@ -1,6 +1,4 @@
-import { Dispatch, FC, SetStateAction } from "react";
-import IdAndClassInput from "@/components/input/idAndClassInput";
-import InputLabelW15 from "@/components/labels/inputLabelW15";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 
 type Props = {
   setTagId: Dispatch<SetStateAction<string>>;
@@ -8,17 +6,59 @@ type Props = {
 }
 
 const IdAndClass:FC<Props> = ({ setTagId, setTagClass }) => {
+  const [idError, setIdError] = useState<string>('');
+  const [classError, setClassError] = useState<string>('');
+
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const pattern = /^[A-Za-z][A-Za-z0-9_-]*$/;
+    if (value !== '' && /\s/.test(value)) {
+      setIdError('idにホワイトスペース(空白やタブなど)を含めることは出来ません');
+    } else if (value !== '' && !pattern.test(value)) {
+      setIdError('先頭に数字を含まない半角英数字、ハイフン(-)、アンダースコア(_)を使用した文字列にしてください');
+    } else {
+      setIdError('');
+    }
+    setTagId(value);
+  }
+
+  const handleClassChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const pattern = /^[A-Za-z][A-Za-z0-9_-]*$/;
+    if (value !== '' && !pattern.test(value)) {
+      setClassError('先頭に数字を含まない半角英数字、ハイフン(-)、アンダースコア(_)を使用した文字列にしてください');
+    } else if (value !== '' && /[ ]{2,}/.test(value)) {
+      setClassError('連続したスペースを含めないようにしてください');
+    } else {
+      setClassError('');
+    }
+    setTagClass(value);
+  }
 
   return(
     <div className="w-full">
-      <div className="flex flex-row mb-1">
-        <InputLabelW15 labelTitle="id" />
-        <IdAndClassInput setState={setTagId} />
+      <div className="flex flex-row my-1">
+        <label className="mr-1.5">id :</label>
+        <input
+          className="border border-black px-1 rounded w-1/2 dark:bg-gray-700 dark:border-gray-500 dark:text-gray-300"
+          onChange={(e) => handleIdChange(e)}
+          type="text"
+        />
       </div>
-      <div className="flex flex-row mb-1">
-        <InputLabelW15 labelTitle="class" />
-        <IdAndClassInput setState={setTagClass} />
+      {idError !== '' &&
+        <p className="text-xs text-red-500 my-1">{idError}</p>
+      }
+      <div className="flex flex-row my-1">
+        <label className="mr-1.5">class :</label>
+        <input
+          className="border border-black px-1 rounded w-1/2 dark:bg-gray-700 dark:border-gray-500 dark:text-gray-300"
+          onChange={(e) => handleClassChange(e)}
+          type="text"
+        />
       </div>
+      {classError !== '' &&
+        <p className="text-xs text-red-500 my-1">{classError}</p>
+      }
     </div>
   );
 }
