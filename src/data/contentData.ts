6d1a8e1657e1120f08246ce8767ba166f56a7322
kazu51-gh <kazu51git@gmail.com>
2023-11-24 19:20:46 +0900

    redesign code factory system

---
+++ src/data/contentData.ts
@@ -0,0 +1,12 @@
+export const contentData: Record<string, string> = {
+  a: 'リンクとして表示するテキスト',
+  blockquote: '引用文',
+  button: 'ボタンに表示するテキスト',
+  code: 'コード',
+  div: 'コンテンツ',
+  h: '見出し',
+  p: 'テキスト',
+  span: 'コンテンツ',
+  strong: '強調するテキスト',
+  textarea: '初期テキスト',
+}
---
+++ src/data/elementData.ts
@@ -0,0 +1,29 @@
+export const headingElements: string[] = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+];
+
+export const voidElements: string[] = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'source',
+  'track',
+  'wbr',
+];
+
+export const listElements: string[] = [
+  'ol',
+  'ul',
+];
