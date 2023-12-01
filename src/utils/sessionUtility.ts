bf5cd366b56db65758c724aab4a6d294b466be1c
kazu51-gh <kazu51git@gmail.com>
2023-12-01 16:27:36 +0900

    add description

--- src/utils/sessionUtility.ts
+++ src/utils/sessionUtility.ts
@@ -1,4 +1,14 @@
+/**
+ * sessionStorageに関するユーティリティクラス
+ */
 export class SessionUtility extends Object {
+
+  /**
+   * sessionStorageに格納されているデータを取得する
+   * @param key キー
+   * @param defaultData 既定値
+   * @returns 保持データ
+   */
   public static getStoredData(key: string, defaultData: string): string {
     if (typeof sessionStorage !== 'undefined') {
       return(sessionStorage.getItem(key) || defaultData);
